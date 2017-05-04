7f5496663d42401f4989632708b0439277b104a7
Niko <wohlgemuth.niko@gmail.com>
2017-05-04 09:13:52 +0200

    new gb entry and tiny style changes

--- src/app/shared/services/gb-storage.service.ts
+++ src/app/shared/services/gb-storage.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 
-import { GbEntry } from "../models/gb-entry.model";
+import { GbEntry } from '../models/gb-entry.model';
 
 @Injectable()
 export class GbStorage {
@@ -15,11 +15,15 @@ export class GbStorage {
     }
 
     private createEntries(): void {
-        let entry: GbEntry;
-        entry = new GbEntry();
-        entry.setContent("Frau Wohlgemuth von Idee & Idyll  hat im Jahr 2015 mit viel Gespür, gutem Einfühlungsvermögen, großer  Erfahrung, hervorragender Kompetenz im grünen Bereich und unendlicher Liebe zum Gestalten mit der Natur eine nach unseren Wünschen und Vorstellungen individuelle Gartenumgestaltung geplant. Für die Planung und Begleitung bei der Umsetzung von ganz persönlichen Gartenträumen  können wir sie wärmstens weiterempfehlen.")
-            .setAuthor("Familie Schramek")
-            .setLocation("Landshut");
-        this.entries.push(entry);
+        const entry1 = new GbEntry();
+        entry1.setContent('Frau Wohlgemuth von Idee & Idyll  hat im Jahr 2015 mit viel Gespür, gutem Einfühlungsvermögen, großer  Erfahrung, hervorragender Kompetenz im grünen Bereich und unendlicher Liebe zum Gestalten mit der Natur eine nach unseren Wünschen und Vorstellungen individuelle Gartenumgestaltung geplant. Für die Planung und Begleitung bei der Umsetzung von ganz persönlichen Gartenträumen  können wir sie wärmstens weiterempfehlen.')
+            .setAuthor('Familie Schramek')
+            .setLocation('Landshut');
+        this.entries.push(entry1);
+        const entry2 = new GbEntry();
+        entry2.setContent('Als junge Bauherren hatten wir nicht viel Ahnung davon, wie man einen Garten anlegt. Daher hatten wir uns im Jahr 2013, aufgrund von Empfehlungen, an Frau Wohlgemuth gewandt. Sie hat uns einen wunderschönen Plan gezeichnet und uns damit tolle Ideen zur Gartengestaltung gegeben. Auch bei bei der richtigen Auswahl der Pflanzen und deren sinnvollen Anordnung im Garten hat Frau Wohlgemuth uns kompetent beraten und geholfen. So ist mit ihrer Hilfe ein toller Garten entstanden, in dem alles wunderbar aufeinander abgestimmt ist. Vor allem ist er etwas Besonderes geworden und kein Standartgarten. Wir und die Kinder fühlen uns darin sehr wohl. Frau Wohlgemuth steht uns auch jetzt noch bei Fragen zur weiteren Ausgestaltung des Gartens oder zu den Pflanzen immer wieder mit Rat und Tat zur Seite, was wir an ihrer Arbeit sehr schätzen.')
+            .setAuthor('Familie Krämer')
+            .setLocation('Mirskofen');
+        this.entries.push(entry2);
     }
 }
