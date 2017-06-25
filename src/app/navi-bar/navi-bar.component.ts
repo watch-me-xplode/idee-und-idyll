cbc4e71d4f86a6c50a3d9b51e24b5e886ed50a66
Niko <wohlgemuth.niko@gmail.com>
2017-06-25 13:42:14 +0200

    hover effect for links

--- src/app/navi-bar/navi-bar.component.ts
+++ src/app/navi-bar/navi-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navi-bar',
